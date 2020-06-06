34e7aad922602f08b14845edac9a9830d5e7e452
Alex Pestell <alex.pestell@gmail.com>
2020-06-06 23:09:37 +0200

    Save logged-in state to sessionStorage.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Login from './components/Login';
 import apiClient from './services/api';
 
 const App = () => {
-  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [loggedIn, setLoggedIn] = React.useState(
+    sessionStorage.getItem('loggedIn') == 'true' || false
+  );
   const login = () => {
     setLoggedIn(true);
+    sessionStorage.setItem('loggedIn', true);
   };
   const logout = () => {
     apiClient.post('/logout').then(response => {
       if (response.status === 204) {
         setLoggedIn(false);
+        sessionStorage.setItem('loggedIn', false);
       }
     })
   };
